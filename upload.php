d1677cd97b3a508fb418d70e58d7f2bccb3f9875
sitekk22 <sitarzkam100@gmail.com>
2023-03-08 18:53:58 +0100

    img verify

--- upload.php
+++ upload.php
@@ -6,16 +6,21 @@ $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if (isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  /* if ($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+  if ($check !== false) {
+    echo "Typ pliku- " . $check["mime"] . ",\n";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo "Dozwolone sa tylko zdjecia.\n";
     $uploadOk = 0;
-  } */
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "Plik " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " został wrzucony na serwer.";
+  }
+  if ($uploadOk == 0) {
+    echo "Nie udało się wrzucić pliku.";
+    // if everything is ok, try to upload file
   } else {
-    echo "Błąd";
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "\"" . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . "\" został wrzucony na serwer.";
+    } else {
+      echo "Błąd.";
+    }
   }
 }
